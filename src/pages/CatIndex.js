b0a817f0d81e90e5c86295808c4dbbbd2db7efbc
ArtyG-10 <a.gourent@outlook.com>
2021-08-24 19:45:30 -0400

    cleaning up syntax

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -8,21 +8,20 @@ class CatIndex extends Component {
     return (
       <>
         <h2>Meet the Cats!</h2>
-<br />
-  <Col sm="6">
-    { this.props.cats.map(cat => {
-      return (
-        <Card body key={ cat.id }>
-          <CardTitle>
-          <NavLink to={`/catshow/${cat.id}`}>
-            { cat.name }
-          </NavLink>
-
-          </CardTitle>
-        </Card>
-      )
-    })}
-  </Col>
+        <br />
+        <Col sm="6">
+          {this.props.cats && this.props.cats.map(cat => {
+            return (
+            <Card body key={ cat.id }>
+              <CardTitle>
+              <NavLink to={`/catshow/${cat.id}`}>
+                { cat.name }
+              </NavLink>
+              </CardTitle>
+            </Card>
+            )
+          })}
+        </Col>
 
       </>
     )
--- src/pages/__tests__/CatIndex.test.js
+++ src/pages/__tests__/CatIndex.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import  Adapter  from 'enzyme-adapter-react-16';
 import CatIndex from '../CatIndex.js';
+
 Enzyme.configure({ adapter: new Adapter() });
 
 
 describe('When the home page loads', () => {
     it('has an p tag', () => {
-      expect(shallow(<CatIndex />).find('Navlink').length).toEqual(1)
+      expect(shallow(<CatIndex />).find('CardTitle').length).toEqual(1)
     })
   })
-  
