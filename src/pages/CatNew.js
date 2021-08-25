6b7c447b847777d705b9cfd90dcd00a15fff0bdd
ArtyG-10 <a.gourent@outlook.com>
2021-08-25 17:24:35 -0400

    implemented handlesubmit and redirect after submit

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
 
 class CatNew extends Component {
   constructor(props){
@@ -9,36 +10,49 @@ class CatNew extends Component {
         name: '',
         age: '',
         enjoys: ''
-      }
+      },
+      submitted: false
     }
   }
 
   handleChange = (e) => {
-    console.log(e.target.value)
-    // let {form } = this.state
-    // form[e.target.name] = e.target.value
-    // this.setState({ form: form })
+    let {form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({ form: form })
+  }
+
+  handleSubmit = () => {
+    this.props.createNewCat(this.state.form)
+    this.setState({submitted:true})
   }
 
   render() {
     console.log(this.state.form)
     return (
       <>
-        <h3>I am the add new cat page</h3>
+        <h3>Add a new cat!</h3>
         <Form>
          <FormGroup>
            <Label>Name</Label>
-           <Input type="text" name="name" onChange={ this.handleChange }/>
+           <Input type="text" name="name" value={this.state.form.name} onChange={ this.handleChange }/>
          </FormGroup>
          <FormGroup>
            <Label>Age</Label>
-           <Input type="text" name="age" onChange={ this.handleChange }/>
+           <Input type="text" name="age" value={this.state.form.age} onChange={ this.handleChange }/>
          </FormGroup>
          <FormGroup>
            <Label>Enjoys</Label>
-           <Input type="text" name="enjoys" onChange={ this.handleChange }/>
+           <Input type="text" name="enjoys" value={this.state.form.enjoys} onChange={ this.handleChange }/>
          </FormGroup>
         </Form>
+        <button
+          type="submit"
+          value=""
+          onClick={ this.handleSubmit }
+        >
+        Submit Profile
+        </button>
+        {this.state.submitted && <Redirect to='/catindex' />}
       </>
     )
   }
